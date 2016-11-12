2f7fa72ce89df09131116cb2e173e8fd9bfe2691
mdsecurity <mattiasduffy@MacBook-Woah.local>
2016-11-12 14:17:05 -0500

    added the bitmap

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,18 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+var image = UIImage
 
 var str = "Hello, playground"
+var map = [[Int]]();
+
+for x in 0...999{
+    for y in 0...999{
+        map[x][y] = 0
+    }
+}
+
+class navigate_map {
+    var map = [[Int]]();
+    
+}
