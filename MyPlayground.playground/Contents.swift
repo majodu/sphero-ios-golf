e1acc36e72d5fafe321edfb71500fe701c9256b9
mdsecurity <mattiasduffy@MacBook-Woah.local>
2016-11-12 14:50:19 -0500

    adding pixel reading function

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,8 +5,72 @@
 
 import UIKit
 
-var image = UIImage()
+var image = UIImage(named:"map");
 
+func image(fromPixelValues pixelValues: [UInt8]?, width: Int, height: Int) -> CGImage?
+{
+    var imageRef: CGImage?
+    if var pixelValues = pixelValues {
+        let bitsPerComponent = 8
+        let bytesPerPixel = 1
+        let bitsPerPixel = bytesPerPixel * bitsPerComponent
+        let bytesPerRow = bytesPerPixel * width
+        let totalBytes = height * bytesPerRow
+        
+        imageRef = withUnsafePointer(to: &pixelValues, {
+            ptr -> CGImage? in
+            var imageRef: CGImage?
+            let colorSpaceRef = CGColorSpaceCreateDeviceGray()
+            let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue).union(CGBitmapInfo())
+            let data = UnsafeRawPointer(ptr.pointee).assumingMemoryBound(to: UInt8.self)
+            let releaseData: CGDataProviderReleaseDataCallback = {
+                (info: UnsafeMutableRawPointer?, data: UnsafeRawPointer, size: Int) -> () in
+            }
+            
+            if let providerRef = CGDataProvider(dataInfo: nil, data: data, size: totalBytes, releaseData: releaseData) {
+                imageRef = CGImage(width: width,
+                                   height: height,
+                                   bitsPerComponent: bitsPerComponent,
+                                   bitsPerPixel: bitsPerPixel,
+                                   bytesPerRow: bytesPerRow,
+                                   space: colorSpaceRef,
+                                   bitmapInfo: bitmapInfo,
+                                   provider: providerRef,
+                                   decode: nil,
+                                   shouldInterpolate: false,
+                                   intent: CGColorRenderingIntent.defaultIntent)
+            }
+            
+            return imageRef
+        })
+    }
+    
+    return imageRef
+}
+
+func pixelValues(fromCGImage imageRef: CGImage?) -> (pixelValues: [UInt8]?, width: Int, height: Int)
+{
+    var width = 0
+    var height = 0
+    var pixelValues: [UInt8]?
+    if let imageRef = imageRef {
+        width = imageRef.width
+        height = imageRef.height
+        let bitsPerComponent = imageRef.bitsPerComponent
+        let bytesPerRow = imageRef.bytesPerRow
+        let totalBytes = height * bytesPerRow
+        
+        let colorSpace = CGColorSpaceCreateDeviceGray()
+        var intensities = [UInt8](repeating: 0, count: totalBytes)
+        
+        let contextRef = CGContext(data: &intensities, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: 0)
+        contextRef?.draw(imageRef, in: CGRect(x: 0.0, y: 0.0, width: CGFloat(width), height: CGFloat(height)))
+        
+        pixelValues = intensities
+    }
+    
+    return (pixelValues, width, height)
+}
 var map = [[Int]]();
 
 for x in 0...999{
