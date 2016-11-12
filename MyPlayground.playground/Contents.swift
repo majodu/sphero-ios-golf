3e02cdab86caaeda38915ffa5df7a27052381878
Eyad Hasan <ehasan3@gmu.edu>
2016-11-12 14:37:49 -0500

    Swift. what is swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,25 +4,31 @@
 // pass' back if it got into a hole.
 
 import UIKit
-var image = UIImage()
 
-var map = [[Int]]();
+var map=[[Int]]();
+
+struct hole {
+    
+    
+}
 
 for x in 0...999{
     for y in 0...999{
-        map[x][y] = 0
+        map[x][y]=0
     }
 }
 
 class navigate_map {
     var map = [[Int]]();
-	
+    var image = UIImage()
+    
+    
 	init(chosenMap: [[Int]]) {
 		map = chosenMap
 	}
 	
-	func puttGolfBallTo(x: Double, y: Double) -> (success: Bool, stop: Bool) {
-		
+	func puttGolfBallTo(x: Int, y: Int) -> (success: Bool, stop: Bool) {
+        
 		return (false, false)
 	}
 
